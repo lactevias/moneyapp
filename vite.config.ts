4f72b12a1f5dc072d9732e4fcf3f74c85569db24
lactevias <lactevias@gmail.com>
2025-10-20 14:02:15 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
+// [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Это правило говорит Vite, что "@" - это папка "client/src"
+      // Эта строка - ключ к решению проблемы.
+      // Она говорит Vite, что "@" - это псевдоним для папки "client/src"
       '@': path.resolve(__dirname, './client/src'),
     },
   },
-})
+});
